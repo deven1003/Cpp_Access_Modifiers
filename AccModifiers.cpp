3ecd158c736af4500d649dd81ad61704726bb6e7
DEVEN KALATHIYA <89897347+deven1003@users.noreply.github.com>
2022-09-13 23:18:53 +0530

    Add files via upload

---
+++ AccModifiers.cpp
@@ -0,0 +1,22 @@
+class Patient {
+
+    private:
+        int patientNumber;
+        string diagnosis;
+
+    public:
+
+      void billing() {
+          // code
+      }
+
+      void makeAppointment() {
+          // code
+      }
+}; 
+
+.....................................
+Specifiers	Same Class	Derived Class	Outside Class
+public	....Yes	........Yes	.............Yes
+private	....Yes	........No...............No
+protected	Yes	........Yes..............No
